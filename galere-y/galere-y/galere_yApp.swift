af7e900ac4f40a5167ce254668faa299ea55b38e
Jamie Brannan <jamiebrannan2@gmail.com>
2022-11-18 16:18:22 +0100

    New: Empty project

---
+++ galere-y/galere-y/galere_yApp.swift
@@ -0,0 +1,17 @@
+//
+//  galere_yApp.swift
+//  galere-y
+//
+//  Created by Jamie Brannan on 18/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct galere_yApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
