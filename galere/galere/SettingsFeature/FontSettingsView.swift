6c8276169f71230bde9f0df059a8ea3568e7c32e
Jamie Brannan <jamiebrannan2@gmail.com>
2024-03-05 19:53:32 +0100

    Feat: Create a font pairing display view

--- galere/galere/SettingsFeature/FontSettingsView.swift
+++ galere/galere/SettingsFeature/FontSettingsView.swift
@@ -9,10 +9,40 @@ import SwiftUI
 
 struct FontSettingsView: View {
     var body: some View {
-        Text("Hello, these are fonts!")
+      VStack(alignment: .leading, spacing: 16) {
+        PairingDisplayView()
+        
+        Divider()
+        
+        
+      }
+      .navigationTitle("Font Settings")
+      .navigationBarTitleDisplayMode(.large)
     }
 }
 
+struct PairingDisplayView: View {
+  @State var displaySample = "Display"
+  @State var bodySample = "Body"
+  
+  var body: some View {
+    VStack(alignment: .leading) {
+      Group {
+        TextField("display", text: $displaySample)
+        TextField("body", text: $bodySample)
+      }
+      .padding()
+    }
+    .overlay(
+      RoundedRectangle(cornerSize: CGSize(width: 20, height: 20))
+        .strokeBorder()
+    )
+    .padding()
+  }
+}
+
 #Preview {
+  NavigationView {
     FontSettingsView()
+  }
 }
