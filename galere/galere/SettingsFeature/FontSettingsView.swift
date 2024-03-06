eefe5e5aa1f8d4545c46b7be4339714c11f27c4c
Jamie Brannan <jamiebrannan2@gmail.com>
2024-03-06 11:44:01 +0100

    Feat: Constrain editability of text style to header

--- galere/galere/SettingsFeature/FontSettingsView.swift
+++ galere/galere/SettingsFeature/FontSettingsView.swift
@@ -14,31 +14,75 @@ import SwiftUI
 ///  - Returns: An interactive control center for font calibration while remaining coherent with Dynamic Type standards
 ///
 struct FontSettingsView: View {
+  @State var displayStyle: Font = .title
+  
     var body: some View {
       VStack(alignment: .leading, spacing: 16) {
-        PairingDisplayView()
-        
+
+        // MARK: - Editable Preview
+        PairingDisplayView(
+          displayStyle: $displayStyle
+        )
+
         Divider()
-        
-        
+
+        // MARK: - Fields
+        VStack(alignment: .leading, spacing: 24) {
+          
+          Text("Headers")
+            .font(.title2)
+          
+          FieldDescriptorView(
+            description: "Chose an Apple Text Style",
+            field: {
+              Text("placeholder for selector field")
+            }
+          )
+          
+          FieldDescriptorView(
+            description: "Pick font file",
+            field: {
+              Text("placeholder for selector field")
+            }
+          )
+          
+          Text("Body")
+            .font(.title2)
+        }
+        .padding()
+        Spacer()
       }
       // TODO: Add a save button in the navbar
-      // QUESTION : What should I do for saving settings like font and beyond? Preloaded selectable parings then user defaults right?
+      // ? : - What should I do for saving settings like font and beyond? Preloaded selectable parings then user defaults right?
       .navigationTitle("Font Settings")
       .navigationBarTitleDisplayMode(.large)
     }
 }
+#Preview {
+  NavigationView {
+    FontSettingsView()
+  }
+}
 
 /// The editable text area view that provides a neutral preview of uniquely the fonts
 struct PairingDisplayView: View {
+  @Binding var displayStyle: Font
   @State var displaySample = "Display"
   @State var bodySample = "Body"
   
   var body: some View {
     VStack(alignment: .leading) {
       Group {
-        TextField("display", text: $displaySample)
-        TextField("body", text: $bodySample)
+        TextField(
+          "display",
+          text: $displaySample
+        )
+        .font(displayStyle)
+        TextField(
+          "body",
+          text: $bodySample
+        )
+        .font(.body)
       }
       .padding()
     }
@@ -51,7 +95,7 @@ struct PairingDisplayView: View {
 }
 
 #Preview {
-  NavigationView {
-    FontSettingsView()
-  }
+  PairingDisplayView(
+    displayStyle: .constant(.largeTitle)
+  )
 }
