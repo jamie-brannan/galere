9903f2b99b1766c5748d59b80e536067e3c89756
Jamie Brannan <jamiebrannan2@gmail.com>
2024-03-12 22:21:04 +0100

    Fix: Allow for text wrapping of label and broader alignment settings

--- galere/galere/Common/FieldDescriptorView.swift
+++ galere/galere/Common/FieldDescriptorView.swift
@@ -9,16 +9,20 @@ import SwiftUI
 
 struct FieldDescriptorView<Content: View>: View {
   var description: String
+  var fieldAlignment: HorizontalAlignment = .leading
+  var helperTextAlignment: TextAlignment = .trailing
   @ViewBuilder var field: Content
 
     var body: some View {
-      VStack(alignment: .leading, spacing: 8) {
+      VStack(alignment: fieldAlignment, spacing: 8) {
         HStack{
           Spacer()
           Text(description)
             .font(.caption)
             .foregroundColor(.secondary)
-            .lineLimit(0)
+            .fixedSize(horizontal: false, vertical: true)
+            .lineLimit(nil)
+            .multilineTextAlignment(helperTextAlignment)
         }
         field
       }
@@ -26,5 +30,5 @@ struct FieldDescriptorView<Content: View>: View {
 }
 
 #Preview {
-  FieldDescriptorView(description: "cool will happen", field: { Text("heyeyyyeyey") } )
+  FieldDescriptorView(description: "cool will happen again again again again again again again again again again again again again again", field: { Text("What could we put here eh?") } )
 }
