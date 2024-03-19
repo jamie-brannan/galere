8925294bf1234de26b85db64af945a1288853e1b
Jamie Brannan <jamiebrannan2@gmail.com>
2024-03-19 17:28:18 +0100

    Clean: Lint spacing

--- galere/galere/galereApp.swift
+++ galere/galere/galereApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct GalereApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView(settingsStore: SettingsStore())
-        }
+  var body: some Scene {
+    WindowGroup {
+      ZStack {
+        ContentView(settingsStore: SettingsStore())
+        
+      }
     }
+  }
 }
