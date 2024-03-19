31443d98fe0e987005e2ff71eeae480b9daf42df
Jamie Brannan <jamiebrannan2@gmail.com>
2024-03-19 13:18:40 +0100

    Feat: Loaded saved settings between app lifecycles

--- galere/galere/SettingsFeature/SettingsStore.swift
+++ galere/galere/SettingsFeature/SettingsStore.swift
@@ -11,6 +11,8 @@ import SwiftUI
 
 /// The object from which changes to the global app settings are to be published and subscribed to
 class SettingsStore: ObservableObject {
+  let saveUrl = URL(fileURLWithPath: "MySettings", relativeTo: FileManager.documentsDirectoryURL).appendingPathExtension("json")
+
   @Published var settings: Settings {
     didSet {
       saveSettingsToDisk()
@@ -18,35 +20,62 @@ class SettingsStore: ObservableObject {
   }
 
   init() {
-    guard let taskJsonUrl = Bundle.main.url(forResource: "default", withExtension: "json") else {
-//    guard let taskJsonUrl = URL(filePath: "default").appendingPathExtension("json") else {
-      self.settings = Settings(font: .pirata, size: 14)
-      return
-    }
-
+    // TODO: check for a `MySettings.json` first before defaulting
     let decoder = JSONDecoder()
 
+    /// See if we already have a saved setting
+    guard FileManager.default.fileExists(atPath: saveUrl.path) else {
+
+      /// Or else get the default json setting
+      guard let taskJsonUrl = Bundle.main.url(forResource: "default", withExtension: "json") else {
+        self.settings = Settings(font: .pirata, size: 14)
+        return
+      }
+
+      do {
+        let taskData = try Data(contentsOf: taskJsonUrl)
+        let task = try decoder.decode(Settings.self, from: taskData)
+        self.settings = task
+        return
+      } catch let error {
+        //      #if DEBUG
+        print("\(error)") // TODO: chose logging/reporting tool
+        //      #endif
+        self.settings = Settings(font: .pirata, size: 108) // TODO: Something else hardcoded
+        return
+      }
+    }
+
+    /// Decode our saved json
     do {
-      let taskData = try Data(contentsOf: taskJsonUrl)
+      let taskData = try Data(contentsOf: saveUrl)
       let task = try decoder.decode(Settings.self, from: taskData)
       self.settings = task
-      return
     } catch let error {
-//      #if DEBUG
-      print("\(error)") // TODO: chose logging/reporting tool
-//      #endif
-      self.settings = Settings(font: .pirata, size: 108) // TODO: Something else hardcoded
-      return
+      print(error)
+      self.settings = Settings(font: .pirata, size: 108)
     }
   }
 
+  /// Simply read to see if the user has already saved settings before on this device
+  func isAlreadySaved() -> Bool {
+    let check = FileManager.default.fileExists(atPath: "MySettings.json")
+    return check
+  }
+
+  func loadJson(from url: URL) {
+    
+  }
+  /// When the user has expressed their own view of settings (lumped together for now) write into a json file for now
+  /// In a fully scaled project, would be stored in a back-end user account of some kind that would write information to a data base
+  // TODO: Mark a way in DocC future path ideas with larger scope and infrastructure
+  // TODO: Seperate encode and writing the encoded json?
   private func saveSettingsToDisk() {
     let encoder = JSONEncoder()
 
     do {
       let taskData = try encoder.encode(settings)
-      let taskUrl = URL(fileURLWithPath: "MySettings", relativeTo: FileManager.documentsDirectoryURL).appendingPathExtension("json")
-      try taskData.write(to: taskUrl)
+      try taskData.write(to: saveUrl)
     } catch let error {
       print(error)
     }
