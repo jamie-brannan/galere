62eb660dc563f1106ee58dd7f6f33adcdcc3f077
Jamie Brannan <jamiebrannan2@gmail.com>
2024-03-19 17:49:34 +0100

    Feat: Add a stateful static screen that disappears

--- galere/galere/Resources/LaunchScreen.swift
+++ galere/galere/Resources/LaunchScreen.swift
@@ -4,13 +4,34 @@
 //
 //  Created by Jamie Brannan on 19/03/2024.
 //
-
+import Foundation
 import SwiftUI
+
+/// Integration inspired by : https://holyswift.app/animated-launch-screen-in-swiftui/
+
+enum LaunchScreenStep {
+  case start
+  case finish
+}
+
+final class LaunchScreenStateManager: ObservableObject, Observable {
+  @MainActor @Published private(set) var state: LaunchScreenStep = .start
+
+  @MainActor func dismiss() {
+    Task {
+      try? await Task.sleep(for: Duration.seconds(1))
+      self.state = .finish
+    }
+  }
+}
 
 struct LaunchScreen: View {
   var body: some View {
-    Text("Galère")
-      .font(.addedFonts(.jacquarda, size: 100, relativeTo: .title))
+    ZStack {
+      Color.mint
+      Text("Galère")
+        .font(.addedFonts(.jacquarda, size: 100, relativeTo: .title))
+    }.ignoresSafeArea()
   }
 }
 
--- galere/galere/galereApp.swift
+++ galere/galere/galereApp.swift
@@ -9,12 +9,16 @@ import SwiftUI
 
 @main
 struct GalereApp: App {
+  @StateObject var launchScreenState = LaunchScreenStateManager()
+
   var body: some Scene {
     WindowGroup {
       ZStack {
         ContentView(settingsStore: SettingsStore())
-        
-      }
+        if launchScreenState.state != .finish {
+          LaunchScreen()
+        }
+      }.environmentObject(launchScreenState)
     }
   }
 }
