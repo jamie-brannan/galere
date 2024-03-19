537e38382d87261b90d0ade9306ff8cdb9c82d39
Jamie Brannan <jamiebrannan2@gmail.com>
2024-03-19 17:19:44 +0100

    Feat: Add splash screen and move assets

---
+++ galere/galere/Resources/LaunchScreen.swift
@@ -0,0 +1,19 @@
+//
+//  LaunchScreen.swift
+//  galere
+//
+//  Created by Jamie Brannan on 19/03/2024.
+//
+
+import SwiftUI
+
+struct LaunchScreen: View {
+  var body: some View {
+    Text("Galère")
+      .font(.addedFonts(.jacquarda, size: 100, relativeTo: .title))
+  }
+}
+
+#Preview {
+  LaunchScreen()
+}
