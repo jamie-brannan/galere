4f5b6e1e1a9e3bd619a043b47865b009958d64fa
Jamie Brannan <jamiebrannan2@gmail.com>
2024-03-20 14:51:31 +0100

    Doc: File Manager extension

--- galere/galere/Extensions/FileManagerExtensions.swift
+++ galere/galere/Extensions/FileManagerExtensions.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public extension FileManager {
-  /// Inspired by related Kodeco tutorials coding style
+  /// Inspired by related Kodeco tutorials coding style, this is the root of the users data written on their desk related to Galère
   static var documentsDirectoryURL: URL {
     return `default`.urls(for: .documentDirectory, in: .userDomainMask)[0]
   }
