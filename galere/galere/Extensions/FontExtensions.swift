d306bd032bebbe2f1a52211688a5cab6caa62678
Jamie Brannan <jamiebrannan2@gmail.com>
2024-03-20 13:57:36 +0100

    Doc: Clean and add documentation and citations

--- galere/galere/Extensions/FontExtensions.swift
+++ galere/galere/Extensions/FontExtensions.swift
@@ -11,17 +11,32 @@ import SwiftUI
 
 public extension Font {
 
+  /// Font file names that have been added to the `xcproj` from Google Fonts
   enum CustomFonts: String, CaseIterable, Codable {
+    /// Source: https://fonts.google.com/specimen/Chewy?classification=Display
     case chewy = "Chewy-Regular"
+    /// Source: https://fonts.google.com/specimen/Jacquarda+Bastarda+9?query=jacquarda&classification=Display
     case jacquarda = "JacquardaBastarda9-Regular"
+    /// Source: https://fonts.google.com/specimen/Micro+5?query=micro+5&classification=Display
     case micro = "Micro5-Regular"
+    /// Source: https://fonts.google.com/specimen/Pirata+One?classification=Display
     case pirata = "PirataOne-Regular"
   }
 
+  /// Using a font added regardless of the ``SettingsStore`` state.
+  ///   - Parameter customFont:Only chose form added font files
+  ///   - Parameter size: Size to render the font in
+  ///   - Parameter relativeTo: Dynamic Type scaling porportion based on Apple's ``Font.TextStyle``
   static func addedFonts(_ customFont: CustomFonts, size: CGFloat?, relativeTo style: Font.TextStyle) -> Font {
     return Font.custom(customFont.rawValue, size: size ?? 16, relativeTo: style)
   }
 
+  /// When using a font programed with ``Settings`` currently in the ``SettingsStore``, only need to pass the store and it's style
+  ///   - Parameter store: The ``SettingsStore`` observed at the time
+  ///   - Parameter relativeTo: Dynamic Type scaling porportion based on Apple's ``Font.TextStyle``
+  ///
+  /// ## In the future
+  ///  The `Font.TextStyle` per font setting may be added in ``Settings`` and this function cound be handled otherwise
   static func settingsBased(_ store: SettingsStore, style: Font.TextStyle) -> Font {
     return Font.custom(store.settings.font.rawValue, size: store.settings.size, relativeTo: style)
   }
@@ -33,44 +48,6 @@ public extension Font {
       Text(sample.rawValue)
         .font(.addedFonts(sample, size: 16, relativeTo: .body))
         .padding()
-    }
-  }
-}
-
-// MARK: - Other
-
-// FIXME: - Needed or not?
-extension Font.TextStyle {
-  var rawValue: String {
-    switch self {
-    case .largeTitle:
-      "largeTitle"
-    case .title:
-      "title"
-    case .title2:
-      "title2"
-    case .title3:
-      "title3"
-    case .headline:
-      "headline"
-    case .subheadline:
-      "subheadline"
-    case .body:
-      "body"
-    case .callout:
-      "callout"
-    case .footnote:
-      "footnote"
-    case .caption:
-      "caption"
-    case .caption2:
-      "caption2"
-    case .extraLargeTitle:
-      "extraLargeTitle"
-    case .extraLargeTitle2:
-      "extraLargeTitle2"
-    @unknown default:
-      fatalError()
     }
   }
 }
