bc0ee50795c03140e91e51958d9c79531e353fa2
Jamie Brannan <jamiebrannan2@gmail.com>
2024-03-27 12:40:21 +0100

    Feat: Create models for links and sources to be cited

---
+++ galere/galere/Models/ExternalLink.swift
@@ -0,0 +1,15 @@
+//
+//  ExternalLink.swift
+//  galere
+//
+//  Created by Jamie Brannan on 27/03/2024.
+//
+
+import Foundation
+
+struct ExternalLink {
+  let iconName: String
+  let displayName: String
+  var description: String?
+  let weblink: URL
+}
---
+++ galere/galere/Models/SourceCited.swift
@@ -0,0 +1,16 @@
+//
+//  SourceCited.swift
+//  galere
+//
+//  Created by Jamie Brannan on 27/03/2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct SourceCited {
+  let name: String
+  let category: String
+  let description: String
+  let links: [ExternalLink]
+}
